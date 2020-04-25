d8e92eb82fc8e93137dffac9789a017f43f9f430
KamaleshR <62994148+KamaleshR@users.noreply.github.com>
2020-04-25 18:00:28 +0530

    Add files via upload

--- lastdigit_sum_fibonacci.py
+++ lastdigit_sum_fibonacci.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[16]:
-
-
-def fibci(n):
-    a=0
-    b=1
-    s=1
-    if n<=1:
-        return n
-    for i in range(n-1):
-        a,b=b,a+b
-        s+=b
-    return s
-
-def digi(n):
-    k=fibci(n)
-    return k%10
-    
-
-n=int(input())
-print(digi(n))
-
-
 # In[ ]:
 
 
+def fibci_sum_lastdigit(n):
+    a=0
+    b=1
+    s=1
+    if(n<=1):
+        return n
+    else:
+        for i in range(n-1):
+            a,b=b%10,(a+b)%10
+            s+=b
+        return s%10
+    
 
+
+n = int(input())
+print(fibci_sum_lastdigit(n))
 
 
 # In[ ]:
